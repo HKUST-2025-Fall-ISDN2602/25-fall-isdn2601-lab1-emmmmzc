45556a1be62a86481571c2cddca36e4b7e71ad6f
LI_Shirui <slifs@connect.ust.hk>
2025-10-05 20:19:03 +0800

    Add files via upload

--- Lab1_I_V_Curve_Measurement_Skeleton.ino
+++ Lab1_I_V_Curve_Measurement_Skeleton.ino
@@ -3,7 +3,7 @@
 #define Vin 12  //*** Define the GPIO pin 12 as Vin ***
 #define Vout 27  //*** Define the GPIO pin 27 as Vout ***
 //#define Cal //*** Define the GPIO pin 14 as the node for current calcultion ***
-
+#define Vmid 14
 //float R = ; //*** Resistance in Mohm ***
 
 float x;
@@ -19,7 +19,7 @@ void setup() {
   
   delay(100);   //*** Set 100 ms delay ***
   //This task only needs to be done once, thus it is placed in setup().
-  Serial.println("duty cycle,Vout "); // display duty cycle,Vout for TASK 1 or I,Vout for Task 2 3 4
+  Serial.println("I,Vout "); // display duty cycle,Vout for TASK 1 or I,Vout for Task 2 3 4
   for (int onPeriod = 0; onPeriod < 256; onPeriod++) 
     {
     
@@ -27,12 +27,15 @@ void setup() {
     int Vout_value = analogRead(Vout); // read the voltage level at Vout 
     // int c_value = analogRead(); // read the voltage level at the node for current calcultion  
     // c=  ;//calculate the vol tage at the node for current calcultion  
-    x = float 
-    (onPeriod)/255 ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
-    y = float(Vout_value)*3.3/4095 ; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
+    // task01: x = float (onPeriod)/255 ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
+    int Vmid_value = analogRead(Vmid);
+    x = float(Vmid_value - Vout_value)*3.3/4095;
+    // task1 y = float(Vout_value)*3.3/4095 ; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
+    y = x*1000 / 2000;
     Serial.print(x); 
     Serial.print(","); // seperate values by comma 
-    Serial.println(y);
+    Serial.print(y);
+    Serial.print("#");
   }
 
   // Press reset button for a new sweep
